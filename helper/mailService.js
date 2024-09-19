7a905673c5002bdc6b3aff76c197a6633693ae10
Mithun <mithuncp398@gmail.com>
2024-09-19 20:43:13 +0530

    mail service added

--- helper/mailService.js
+++ helper/mailService.js
@@ -6,17 +6,18 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail', // You can also use 'hotmail', 'yahoo', etc.
   auth: {
-    user: 'mithun.thavam304@gmail.com', // Your email address
-    pass: 'mithuthavam'   // Your email password or an app-specific password
+    user: process.env.EMAIL, // Your email address
+    pass: process.env.PASSWORD   // Your email password or an app-specific password
   }
 });
+
 
 // Function to send an email
 const sendMail = async (to, subject, text) => {
   try {
     const mailOptions = {
-      from: 'mithun.thavam304@gmail.com', // Sender address
-      to: 'mithuncp398@gmail.com', // List of receivers (can be an array or a single email)
+      from:  process.env.EMAIL, // Sender address
+      to: process.env.EMAIL2, // List of receivers (can be an array or a single email)
       subject: subject, // Subject line
       text: text, // Plain text body
     };
